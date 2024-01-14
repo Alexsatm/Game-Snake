2bfad328a07d854275a7399e4ef19f93d69eab1a
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-14 20:23:31 +0300

    feat: add function gameStart

--- app.js
+++ app.js
@@ -15,10 +15,23 @@ let yVelocity = 0;
 let foodX;
 let foodY;
 let score = 0;
-let snake = [
+let snake = [ //каждый объект эта часть тела змеи
     {x:unitSize * 4, y:0},
     {x:unitSize * 3, y:0},
     {x:unitSize * 2, y:0},
     {x:unitSize, y:0},
     {x:0, y:0}
 ];
+
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
+
+gameStart();
+
+function gameStart(){
+    running= true;
+    scoreText.textContent = score;
+    createFood();
+    drawFood();
+    nextTick();
+};
